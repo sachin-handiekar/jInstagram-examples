5646b7776b8646c3fc3f00662495f4b406b10075
Sachin Handiekar <sach21@gmail.com>
2016-02-20 09:44:17 +0000

    Update Constants.java

--- src/main/java/com/sachinhandiekar/examples/Constants.java
+++ src/main/java/com/sachinhandiekar/examples/Constants.java
@@ -5,9 +5,10 @@ public final class Constants {
     /**
      * Config Properties
      */
-    public static final String CLIENT_ID = "instagram.clientId";
-    public static final String CLIENT_SECRET = "instagram.clientSecret";
-    public static final String REDIRECT_URI = "instagram.redirectUri";
+    public static final String CLIENT_ID = "IG_CLIENT_ID";
+    public static final String CLIENT_SECRET = "IG_CLIENT_SECRET";
+    public static final String REDIRECT_URI = "IG_REDIRECT_URL";
+
 
     /**
      * Http Session Attributes
